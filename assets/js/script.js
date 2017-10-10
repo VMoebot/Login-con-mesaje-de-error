c57d621a535986c7d99dca31c66dbd7370b907d6
Jose Alfredo P.C <32446743+VMoebot@users.noreply.github.com>
2017-10-10 07:39:53 -0500

    :^]
    asda

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,14 @@ function validar_campos(){
     var correo=document.getElementsByName('correo')[0];
     var contraseña=document.getElementsByName('contraseña')[0];
                 
-    var continuar = true;
-    console.log(correo.value);
-    console.log(contraseña.value);
+    var enviar = true;
+   
     if (contraseña.value!=''){
         contraseña.style.border='1px solid #ccc';
         contraseña.style.backgroundColor='#fff';
     }
     else {
-        continuar= false;
+        enviar= false;
         contraseña.style.border ='1px solid red';
         contraseña.style.backgroundColor='#fdf7f9';
     }
@@ -19,14 +18,14 @@ function validar_campos(){
         correo.style.backgroundColor='#fff';
     }
     else {
-        continuar=false;
+        enviar=false;
         correo.style.border='1px solid red';
         correo.style.backgroundColor='#fff';
 
     }
 
-    if(!continuar){
+    if(!enviar){
         document.getElementsByClassName('alert')[0].style.display='block';
     }
-    return continuar;
+    return enviar;
 }
